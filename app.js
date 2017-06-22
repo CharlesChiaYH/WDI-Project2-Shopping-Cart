39e154158375ba9673cd2dd728fb4567f768f84f
CharlesChiaYH <CharlesChiaYH@users.noreply.github.com>
2017-06-22 15:57:48 +0800

    create git ignore and .env file

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var session = require('express-session');
 var passport = require ('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+import dotenv from 'dotenv'
+dotenv.load(path: '.env')
 
 //set MongoStore for 'connect-mongo' middleware package//
 //'connect-mongo' is set up after sessions, and sessions//
@@ -26,7 +28,7 @@ var userRoutes = require('./routes/user');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost:27017/shopping');
+mongoose.connect(process.env.MONGODB_URI);
 
 //Setup Passport methods from passport.js. Binding to variable is not required//
 //this is done to avoid dumping all the code from passport.js into app.js//
